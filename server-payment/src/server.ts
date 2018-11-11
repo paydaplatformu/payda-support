57acb07182ccf24dc426c2378ad92a562712f5d9
Utku Turunç <utku@turunc.me>
2018-11-11 21:15:46 +0100

    Added deployment setup

--- server-payment/src/server.ts
+++ server-payment/src/server.ts
@@ -1,16 +1,17 @@
 import { ContextFunction } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import * as bodyParser from "body-parser";
+import chalk from "chalk";
 import express, { Express } from "express";
 import "express-async-errors";
 import { Container } from "inversify";
+import path from "path";
 import { config } from "./config";
 import { production, test } from "./container";
 import { errorHandler } from "./middleware/errorHandler";
 import { resolvers, typeDefs } from "./schema";
 import { IContextProvider } from "./schema/context";
 import { TYPES } from "./types";
-import chalk from "chalk";
 
 const log = console.log; // tslint:disable-line
 
@@ -22,6 +23,14 @@ const getProfile = (environment: Environment) => {
   }
   return production;
 };
+
+const port = config.get("port");
+const host = config.get("host");
+const environment: Environment = config.get("environment") as any;
+
+const container = new Container();
+const profile = getProfile(environment);
+container.load(profile);
 
 export const createServer = (callback?: (error: any, app: Express) => any) => {
   const context: ContextFunction = async () => {
@@ -44,18 +53,15 @@ export const createServer = (callback?: (error: any, app: Express) => any) => {
   const app: Express = express();
 
   app.use(bodyParser.json());
-
-  server.applyMiddleware({ app }); // app is from an existing express app
-
+  server.applyMiddleware({ app });
   app.use(errorHandler);
 
-  const port = config.get("port");
-  const host = config.get("host");
-  const environment: Environment = config.get("environment") as any;
+  app.use(express.static(path.resolve(__dirname, "../frontend-dist")));
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend-dist", "index.html"));
+  });
 
-  const container = new Container();
-  const profile = getProfile(environment);
-  container.load(profile);
   return app.listen(port, host, () => {
     log(
       `\n  ${chalk.gray("App is running at")} ${chalk.cyan("http://%s:%d")} ${chalk.gray("in")} ${chalk.blue(
