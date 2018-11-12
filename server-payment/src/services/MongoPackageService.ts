038b3b1791000087c8a16431af80566dbedaf665
Utku Turunç <utku@turunc.me>
2018-11-12 13:43:10 +0100

    Fixed production errors

--- server-payment/src/services/MongoPackageService.ts
+++ server-payment/src/services/MongoPackageService.ts
@@ -2,6 +2,6 @@ import { injectable } from "inversify";
 import { IPackageService } from "../models/PackageService";
 
 @injectable()
-export class MongoPackageService implements IPackageService {
+export class MongoPackageService {
   public getAll = async () => [{ name: "asdffaerf" }] as any;
 }
