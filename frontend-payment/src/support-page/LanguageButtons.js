2d1f58269e6b920b27a8977d5ac30ab48fce922d
Anil <aak@hellofresh.com>
2019-02-07 21:20:44 +0100

    Implement icons for language buttons

--- frontend-payment/src/support-page/LanguageButtons.js
+++ frontend-payment/src/support-page/LanguageButtons.js
@@ -3,22 +3,30 @@ import React, { useContext } from "react";
 import { TranslationContext } from "../translations";
 import { LANG_CODES } from "../constants";
 
+import iconSrcTR from "..//static/icons/icon_tr.png";
+import iconSrcEn from "..//static/icons/icon_en.png";
+
+const buttonStyle = { background: "none", border: "none", cursor: "pointer" };
+
 const LanguageButton = ({ langCode }) => {
   const { dispatch } = useContext(TranslationContext);
 
   const actionType = langCode === LANG_CODES.TR ? "setTurkish" : "setEnglish";
+  const iconSrc = langCode === LANG_CODES.TR ? iconSrcTR : iconSrcEn;
 
   return (
-    <button onClick={() => dispatch({ type: actionType })}>{langCode}</button>
+    <button style={buttonStyle} onClick={() => dispatch({ type: actionType })}>
+      <img src={iconSrc} alt={`icon ${langCode}`} />
+    </button>
   );
 };
 
 const LanguageButtons = props => {
   return (
-    <>
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
       <LanguageButton langCode={LANG_CODES.TR} />
       <LanguageButton langCode={LANG_CODES.EN} />
-    </>
+    </div>
   );
 };
 
