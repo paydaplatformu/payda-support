aec37a68afa2280a7f06311b037adba9c164506c
Utku Turunç <me@utku.me>
2019-02-16 20:14:03 +0100

    Rename the database script folder to be more generic and add database copy script

---
+++ script-database/src/copy-database.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const { connect, copyStream } = require("./utils/mongo");
+
+(async () => {
+  try {
+    program
+      .version("0.1.0")
+      .option("-s, --source <url>", "Source database")
+      .option("-t, --target <url>", "Target database")
+      .parse(process.argv);
+
+    const { source, target } = program;
+
+    if (!source) throw new Error("Source must be given.");
+    if (!target) throw new Error("Target must be given.");
+
+    const sourceClient = await connect(source);
+    const targetClient = await connect(target);
+
+    const collections = ["packages", "donations", "subscriptions", "users"];
+
+    const promises = collections.map(collection => {
+      const source = sourceClient.db().collection(collection)
+      const target = targetClient.db().collection(collection)
+      return copyStream(source, target)
+    })
+
+    await Promise.all(promises);
+
+    sourceClient.close();
+    targetClient.close();
+    process.exit(0);
+  } catch (error) {
+    console.error(error.stack);
+    process.exit(1);
+  }
+})();
