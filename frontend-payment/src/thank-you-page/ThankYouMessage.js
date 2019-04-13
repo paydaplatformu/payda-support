2577b837d906567d660e9a27273d099797c39928
Anil <aak@hellofresh.com>
2019-04-13 13:28:34 +0000

    Add Thank You page

---
+++ frontend-payment/src/thank-you-page/ThankYouMessage.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+
+import { TranslationContext } from "../translations";
+import { paydaOrange } from "../constants";
+
+const headingStyle = { textAlign: "center", color: paydaOrange };
+
+const Title = () => {
+  const { translate } = useContext(TranslationContext);
+
+  return (
+    <div>
+      <h1 style={{ ...headingStyle }}>{translate("thank_you_page_title")}</h1>
+      <h4 style={{ ...headingStyle }}>
+        {translate("thank_you_page_description")}
+      </h4>
+    </div>
+  );
+};
+
+export default Title;
