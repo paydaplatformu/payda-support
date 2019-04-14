cab12ce808801466a60b5347d519ea8eb311485e
Anil <aak@hellofresh.com>
2019-04-14 12:02:00 +0200

    Implement login for admin page

---
+++ frontend-payment/src/admin-page/authProvider.js
@@ -0,0 +1,39 @@
+import qs from "qs";
+import { AUTH_LOGIN, AUTH_LOGOUT } from "react-admin";
+import { baseURL } from "../constants";
+
+export default (type, params) => {
+  if (type === AUTH_LOGIN) {
+    const { username, password } = params;
+    const request = new Request(`${baseURL}/oauth2/token`, {
+      method: "POST",
+      body: qs.stringify({
+        grant_type: "password",
+        client_id: "iframe",
+        client_secret: "123456",
+        username,
+        password,
+        scope: "admin",
+      }),
+      headers: new Headers({
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
+    });
+    return fetch(request)
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ accessToken }) => {
+        localStorage.setItem("accessToken", accessToken);
+      });
+  }
+
+  if (type === AUTH_LOGOUT) {
+    localStorage.removeItem("accessToken");
+    return Promise.resolve();
+  }
+  return Promise.resolve();
+};
