6e34b93c59013048f1780f42b76cee6740f6fadb
Utku Turunç <utku@turunc.me>
2019-08-06 20:16:25 +0200

    Enable test intervals again

--- server-payment/src/schema/RepeatInterval.ts
+++ server-payment/src/schema/RepeatInterval.ts
@@ -1,25 +1,11 @@
 import { gql } from "apollo-server-core";
-import { isNonProduction } from "../utilities/helpers";
 
-const getRepeatInterval = () => {
-  if (isNonProduction()) {
-    return gql`
-      enum RepeatInterval {
-        NONE
-        MONTHLY
-        YEARLY
-        TEST_A
-        TEST_B
-      }
-    `;
+export const typeDef = gql`
+  enum RepeatInterval {
+    NONE
+    MONTHLY
+    YEARLY
+    TEST_A
+    TEST_B
   }
-  return gql`
-    enum RepeatInterval {
-      NONE
-      MONTHLY
-      YEARLY
-    }
-  `;
-};
-
-export const typeDef = getRepeatInterval();
+`;
