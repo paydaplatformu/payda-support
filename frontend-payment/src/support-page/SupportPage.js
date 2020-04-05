d5f5d742761df508b071695fa98c21dfb6a897a2
Utku Turunç <utku@turunc.me>
2020-04-05 20:19:26 +0200

    Add payment information to the support page

--- frontend-payment/src/support-page/SupportPage.js
+++ frontend-payment/src/support-page/SupportPage.js
@@ -38,16 +38,39 @@ const StyledFormContainer = styled.div`
   margin: 20px 0;
 `;
 
+const StyledPaymentChannelsContainer = styled.div`
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 3px;
+  margin: 20px 0;
+`;
+
 const SupportPage = () => (
   <>
     <GlobalStyle />
+    <LanguageButtons />
     <Row>
       <Col xs={24} md={{ span: 14, offset: 5 }}>
         <StyledFormContainer>
           <Title />
           <DonationFormWithGql />
         </StyledFormContainer>
-        <LanguageButtons />
+        <StyledPaymentChannelsContainer>
+          <h4>PAYLAŞMA VE DAYANIŞMA PLATFORMU DERNEĞİ</h4>
+          <h4>AKBANK Akmerkez Şubesi</h4>
+          <div>
+            <b>TL: </b>TR 11 0004 6007 6888 8000 0446 71
+          </div>
+          <div>
+            <b>EUR: </b>TR 52 0004 6007 6803 6000 0922 04
+          </div>
+          <div>
+            <b>USD: </b>TR 55 0004 6007 6800 1000 0465 86
+          </div>
+          <div>
+            <b>SWIFT Kodu: </b>AKBKTRİS
+          </div>
+        </StyledPaymentChannelsContainer>
       </Col>
     </Row>
   </>
